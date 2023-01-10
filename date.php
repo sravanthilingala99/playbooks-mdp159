6507c089c3c7aa8010b08ed6931dd3b46e706968
sravanthilingala99 <sravanthilingala99@gmail.com>
2023-01-10 11:23:01 +0000

    no handlers

---
+++ date.php
@@ -0,0 +1,11 @@
+
+<html>
+<body>
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+</body>
+</html>
